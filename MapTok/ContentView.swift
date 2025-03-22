4b769b4eb1a8e1b56f0c005cf1405eecbff74dbe
nirv-n <11nirvaan@gmail.com>
2025-03-21 23:32:30 -0500

    updated

--- MapTok/ContentView.swift
+++ MapTok/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, guys!")
+            Text("Hello, world!")
         }
         .padding()
     }
